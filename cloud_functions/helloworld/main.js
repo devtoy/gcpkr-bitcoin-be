70cae5ab59302f78a6824d6317eebd5abf903cb5
Harry Oh <harry@5004.pe.kr>
2017-09-27 18:42:37 +0900

    add main.js

---
+++ cloud_functions/helloworld/main.js
@@ -0,0 +1,9 @@
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.helloWorld = function helloWorld(req, res) {
+    res.status(200).send('Hello World');
+};
